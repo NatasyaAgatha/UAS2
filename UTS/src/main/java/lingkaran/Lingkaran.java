bdb476582df935fd9e449b965857569b235fbc02
- LENOVO - <- LENOVO -@NatasyaAgatha>
2025-05-13 20:52:08 +0700

    fisrtcommit

---
+++ UTS/src/main/java/lingkaran/Lingkaran.java
@@ -0,0 +1,17 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package lingkaran;
+
+/**
+ *
+ * @author - LENOVO -
+ */
+public class Lingkaran {
+    final double PI = 3.141592;
+
+    public double hitungLuas(double r) {
+        return PI * r * r;
+    }
+}
